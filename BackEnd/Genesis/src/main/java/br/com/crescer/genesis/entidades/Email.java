7b371d84a9dd3a2956655b2f6a780a3fbebb3804
rafaelbarreto <rafael.barreto@cwi.com.br>
2017-07-06 16:05:08 -0300

    feat(Email): adicionado dependencia no POM criado entidade e repositorio Email

---
+++ BackEnd/Genesis/src/main/java/br/com/crescer/genesis/entidades/Email.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.genesis.entidades;
+
+/**
+ *
+ * @author rafael.barreto
+ */
+public class Email {
+    private Colaborador colaborador;
+    private String mensagem;    
+    private String tipo;
+
+    public Colaborador getColaborador() {
+        return colaborador;
+    }
+
+    public void setColaborador(Colaborador colaborador) {
+        this.colaborador = colaborador;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ BackEnd/Genesis/src/main/java/br/com/crescer/genesis/repositorios/EmailRepositorio.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.genesis.repositorios;
+
+import br.com.crescer.genesis.entidades.Colaborador;
+import br.com.crescer.genesis.entidades.Email;
+
+/**
+ *
+ * @author rafael.barreto
+ */
+public class EmailRepositorio {
+    
+    private Email email = new Email();
+    
+    public static String novoEmail(Colaborador colaborador, String mensagem, String tipo){
+        
+        
+        
+        return null;
+    }
+}
