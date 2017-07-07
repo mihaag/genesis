7f933c7574b1227f20a58b662bd22cd7cfb97ce5
rafaelbarreto <rafael.barreto@cwi.com.br>
2017-07-06 23:16:39 -0300

    feat(Feito):implementar crud basico da classe feito (repositorio e service)

---
+++ BackEnd/Genesis/src/main/java/br/com/crescer/genesis/services/FeitoService.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.genesis.services;
+
+import br.com.crescer.genesis.entidades.Feito;
+import br.com.crescer.genesis.repositorios.FeitoRepositorio;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author rafa
+ */
+@Service
+public class FeitoService {
+
+    @Autowired
+    FeitoRepositorio repositorio;
+
+    public List<Feito> buscarTodosFeitos() {
+        return (List<Feito>) repositorio.findAll();
+    }
+
+    public Feito buscarFeitoPorId(long id) {
+        return repositorio.FindById(id);
+    }
+
+    public Feito buscarPorNome(String nome) {
+        return repositorio.FindByNomeContainingIgnoreCase(nome);
+
+    }
+
+    public String cadastrarFeito(Feito feito) {
+        Feito feitoBanco = repositorio.FindById(feito.getId());
+        boolean podeSalvar = (feitoBanco != null) && (feitoBanco.getNome() != feito.getNome());
+        final String mensagem = podeSalvar ? "feito cadastrado com sucesso" : "Feito Ja Cadastrado";
+
+        if (podeSalvar) {
+            repositorio.save(feito);
+            return mensagem;
+        } else {
+            return (mensagem);
+        }
+    }
+    
+    public String atualizarFeito(Feito feito){
+        final String mensagem = "atualizado com sucesso";
+        repositorio.save(feito);
+        return mensagem;
+    }
+
+    public String removerFeito(Feito feito) {
+        final String mensagem = "removido com sucesso";
+        repositorio.delete(feito);
+        return mensagem;
+    }       
+
+}
