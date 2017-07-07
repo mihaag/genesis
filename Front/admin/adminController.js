07e0cb322026d5adb85d048af070ebc968a28c35
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-07-07 10:15:45 -0300

    feat(tela admin): adicionar botões para cadastrar e ver times/feitos

--- Front/admin/adminController.js
+++ Front/admin/adminController.js
@@ -1,4 +1,9 @@
 angular.module('app')
     .controller('adminController', function ($scope, authService, $location, toastr) {
       $scope.isCollapsed = true;
+      $scope.irParaHome = irParaHome;
+
+      function irParaHome() {
+          $location.path('/home');
+      }
 });
