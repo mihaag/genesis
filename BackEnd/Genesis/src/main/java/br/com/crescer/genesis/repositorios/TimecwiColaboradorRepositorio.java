0400d3961fb376cd5a3e4c3f3b8351579a28bee7
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-07-08 23:41:48 -0300

    feat(Time): criar rota e implementar método para inativar Time.

--- BackEnd/Genesis/src/main/java/br/com/crescer/genesis/repositorios/TimecwiColaboradorRepositorio.java
+++ BackEnd/Genesis/src/main/java/br/com/crescer/genesis/repositorios/TimecwiColaboradorRepositorio.java
@@ -1,7 +1,6 @@
 package br.com.crescer.genesis.repositorios;
 
 import br.com.crescer.genesis.entidades.TimecwiColaborador;
-import java.io.Serializable;
 import org.springframework.data.repository.CrudRepository;
 
 /**
