318b63ad5eaecea06f52326a0ff70b880dad13f1
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-07-09 12:08:20 -0300

    feat(perfil): começar diagramação tela de perfil

--- Front/perfil/perfilController.js
+++ Front/perfil/perfilController.js
@@ -1,24 +1,12 @@
 angular.module('app')
   .controller('perfilController', function ($scope, authService, $location, toastr, loginService, homeService) {
-    $scope.colaborador = [{
-      "id": 0,
+    $scope.teste = [{
       "email": "alana.weiss@cwi.com.br",
       "nomecompleto": "Alana Lange Weiss",
       "nascimento": "1998-01-22",
-      "admissao": "2017-01-01",
-      "demissao": null,
-      "sede": 1,
-      "ramal": null,
-      "andar": null,
-      "posicao": null,
-      "descricao": null,
-      "descricaoresumida": null,
-      "situacao": "A",
-      "senha": "1111",
-      "idPermissao": {
-        "id": 1
-      }
-    }]
+      "admissao": "2017-01-01"
+    }];
+    console.log($scope.colaborador);
     $scope.feitos = [{
         "nome": "tetse",
         "imagem": "http://files.softicons.com/download/web-icons/services-flat-icons-by-jozef-krajcovic/png/512x512/consult.png"
@@ -31,7 +19,7 @@ angular.module('app')
         "nome": "tetse 3",
         "imagem": "http://files.softicons.com/download/web-icons/services-flat-icons-by-jozef-krajcovic/png/512x512/consult.png"
       }
-    ]
+    ];
 
     $scope.loadMovies = function(query) {
     return ["alana","mirela","jabel", "rafael" ]
