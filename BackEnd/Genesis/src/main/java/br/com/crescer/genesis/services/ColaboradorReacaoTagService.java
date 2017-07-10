af9ee0e998292e62a9e37a3a5886eb0fb677c70e
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-07-09 22:26:45 -0300

    fix(curtir/descurtir): corrigir logica para poder reagir a uma tag sem reacoes ainda

--- BackEnd/Genesis/src/main/java/br/com/crescer/genesis/services/ColaboradorReacaoTagService.java
+++ BackEnd/Genesis/src/main/java/br/com/crescer/genesis/services/ColaboradorReacaoTagService.java
@@ -14,39 +14,43 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ColaboradorReacaoTagService {
+
     @Autowired
     ColaboradorReacaoTagRepositorio colabReacaoTagRepo;
-    
-    public ColaboradorReacaoTag adicionarCurtir(ColaboradorReacaoModel colabReacaoModel){
-        ColaboradorReacaoTag colabReacaoTag = new ColaboradorReacaoTag();
-         Colaborador colab = colabReacaoModel.getColaborador();
-         ColaboradorTag colabTag = colabReacaoModel.getColaboradorTag();
-          colabReacaoTag = colabReacaoTagRepo.findByIdColaboradorAndIdColaboradortag(colab, colabTag);
-         if(colabReacaoTag == null){
-            colabReacaoTag.setId(colabReacaoModel.getId());
-            colabReacaoTag.setIdColaborador(colabReacaoModel.getColaborador());
-            colabReacaoTag.setIdColaboradortag(colabReacaoModel.getColaboradorTag());
-            colabReacaoTag.setReacao('S');
-         }
-         else{
-             colabReacaoTag.setReacao('S');             
-         }
-        return colabReacaoTagRepo.save(colabReacaoTag);
-    }
-    
-    public ColaboradorReacaoTag adicionarDescurtir(ColaboradorReacaoModel colabReacaoModel){
-         ColaboradorReacaoTag colabReacaoTag = new ColaboradorReacaoTag();
-         Colaborador colab = colabReacaoModel.getColaborador();
-         ColaboradorTag colabTag = colabReacaoModel.getColaboradorTag();
-          colabReacaoTag = colabReacaoTagRepo.findByIdColaboradorAndIdColaboradortag(colab, colabTag);
-         if(colabReacaoTag == null){
-            colabReacaoTag.setId(colabReacaoModel.getId());
-            colabReacaoTag.setIdColaborador(colabReacaoModel.getColaborador());
-            colabReacaoTag.setIdColaboradortag(colabReacaoModel.getColaboradorTag());
-            colabReacaoTag.setReacao('N');
-         }
-         else colabReacaoTag.setReacao('N');
 
+    public ColaboradorReacaoTag adicionarCurtir(ColaboradorReacaoModel colabReacaoModel) {
+        ColaboradorReacaoTag colabReacaoTag = new ColaboradorReacaoTag();
+        Colaborador colab = colabReacaoModel.getColaborador();
+        ColaboradorTag colabTag = colabReacaoModel.getColaboradorTag();
+        colabReacaoTag = colabReacaoTagRepo.findByIdColaboradorAndIdColaboradortag(colab, colabTag);
+        if (colabReacaoTag == null) {
+            ColaboradorReacaoTag aux = new ColaboradorReacaoTag();
+            aux.setId(colabReacaoModel.getId());
+            aux.setIdColaborador(colabReacaoModel.getColaborador());
+            aux.setIdColaboradortag(colabReacaoModel.getColaboradorTag());
+            aux.setReacao('S');
+            return colabReacaoTagRepo.save(aux);
+        }
+        
+        colabReacaoTag.setReacao('S');             
+        return colabReacaoTagRepo.save(colabReacaoTag);
+
+    }
+
+    public ColaboradorReacaoTag adicionarDescurtir(ColaboradorReacaoModel colabReacaoModel) {
+        ColaboradorReacaoTag colabReacaoTag = new ColaboradorReacaoTag();
+        Colaborador colab = colabReacaoModel.getColaborador();
+        ColaboradorTag colabTag = colabReacaoModel.getColaboradorTag();
+        colabReacaoTag = colabReacaoTagRepo.findByIdColaboradorAndIdColaboradortag(colab, colabTag);
+        if (colabReacaoTag == null) {
+            ColaboradorReacaoTag aux = new ColaboradorReacaoTag();
+            aux.setId(colabReacaoModel.getId());
+            aux.setIdColaborador(colabReacaoModel.getColaborador());
+            aux.setIdColaboradortag(colabReacaoModel.getColaboradorTag());
+            aux.setReacao('N');
+            return colabReacaoTagRepo.save(aux);
+        }
+        colabReacaoTag.setReacao('N');
         return colabReacaoTagRepo.save(colabReacaoTag);
     }
 }
