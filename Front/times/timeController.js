c5518a9d7302ca94afea155cf87fdeea050b7ffb
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-07-10 14:02:56 -0300

    feat(header): adicionar dropdown com opção de navegação para usuario logado

--- Front/times/timeController.js
+++ Front/times/timeController.js
@@ -1,4 +1,9 @@
 angular.module('app')
-  .controller('timeController', function ($scope, authService, $location, toastr, timeService, timeColaboradorService) {
-      
+  .controller('timeController', function ($scope, authService, $location, toastr) {
+    $scope.user = {"nome": "Alana Lange Weiss"};
+
+    $scope.status = {
+    isopen: false
+  };
+
   });
