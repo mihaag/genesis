dd14f789327bf7fed36144fae77029b07be9984e
rafaelbarreto <rafael.barreto@cwi.com.br>
2017-07-12 20:48:44 -0300

    (edicaoTimeController): adicionado funcao para trocar owner por membro

--- Front/times/edicaoTimeController.js
+++ Front/times/edicaoTimeController.js
@@ -1,56 +1,56 @@
-angular.module('app').controller('edicaoTimeController', function ($scope, authService, $location, toastr, 
+angular.module('app').controller('edicaoTimeController', function ($scope, authService, $location, toastr,
     $routeParams, timesService, colaboradorService, timeColaboradorService) {
-    
+
     $scope.atualizarTime = atualizarTime;
     $scope.pesquisar = pesquisar;
     $scope.adicionarMembros = adicionarMembros;
-    $scope.adicionarOwner = adicionarOwner;  
-    $scope.removerMenbroDoGrupo = removerMenbroDoGrupo; 
-    $scope.removerOwnerDoGrupo = removerOwnerDoGrupo;    
+    $scope.adicionarOwner = adicionarOwner;
+    $scope.removerMenbroDoGrupo = removerMenbroDoGrupo;
+    $scope.removerOwnerDoGrupo = removerOwnerDoGrupo;
     var membrosOwners = {};
-    var timeAtualizado = {}; 
+    var timeAtualizado = {};
     membrosOwners['membros'] = [];
-    membrosOwners['owners'] = [];  
-    
+    membrosOwners['owners'] = [];
+
 
     buscarTime($routeParams.id);
 
-    function buscarTime(id){
-        
+    function buscarTime(id) {
+
         timesService.buscarTimePorId(id).then(response => {
-            $scope.time = response.data;          
+            $scope.time = response.data;
             $scope.membrosTime = [];
             $scope.ownersTime = [];
-           
+
             timeColaboradorService.procurarColaboradorTime($scope.time)
                 .then(function (response) {
                     var colabs = response.data;
-                    colabs.forEach(function(colab) {
-                        if(colab.tipo === "M"){
+                    colabs.forEach(function (colab) {
+                        if (colab.tipo === "M") {
                             $scope.membrosTime.push(colab);
-                            membrosOwners['membros'].push(colab.idColaborador.id)                            
-                        } else if(colab.tipo === "O"){
+                            membrosOwners['membros'].push(colab.idColaborador.id)
+                        } else if (colab.tipo === "O") {
                             $scope.ownersTime.push(colab);
                             membrosOwners['owners'].push(colab.idColaborador.id)
-                        }                         
+                        }
                     }, this);
-            })
+                })
         })
     };
 
     verificaOwner();
 
     function atualizarTime(time) {
-            time.membros = membrosOwners['membros'];
-            time.owners = membrosOwners['owners'];
-            
-            timesService.atualizarTimes(time)
-                .then(function () {                    
-                    toastr.success('Time atualizado');
-                    $location.path('/admin')
-                }, function () {
-                    toastr.error('Ops... Algo deu errado');
-                })
+        time.membros = membrosOwners['membros'];
+        time.owners = membrosOwners['owners'];
+
+        timesService.atualizarTimes(time)
+            .then(function () {
+                toastr.success('Time atualizado');
+                $location.path('/admin')
+            }, function () {
+                toastr.error('Ops... Algo deu errado');
+            })
     };
 
     function pesquisar(nomeColab) {
@@ -58,55 +58,74 @@ angular.module('app').controller('edicaoTimeController', function ($scope, authS
             .then(function (response) {
                 $scope.pesquisa = response.data;
             })
-    };    
-    
-    
+    };
+
+
     function adicionarMembros(membros) {
-        debugger;     
+        debugger;
         let dados = {}
-        let naoPodeAdicionar = membrosOwners['membros'].includes(membros.id) || membrosOwners['owners'].includes(membros.id);        
-        if(naoPodeAdicionar){
+        let naoPodeAdicionar = membrosOwners['membros'].includes(membros.id) || membrosOwners['owners'].includes(membros.id);
+        if (naoPodeAdicionar) {
             toastr.error('ja vinculado ao time');
             return;
         }
 
-        dados['idColaborador'] = membros;           
-        membrosOwners['membros'].push(membros.id); 
-        $scope.membrosTime.push(dados);       
-        $scope.pesquisa = {};        
-    };    
-    
-     
-    function adicionarOwner(owners) {
-        let dados = {}; 
-        let naoPodeAdicionar = membrosOwners['owners'].includes(owners.id) || membrosOwners['membros'].includes(owners.id);
-        if(naoPodeAdicionar){
-            toastr.error('ja vinculado ao time');
-            return;  
-        }
-
-        dados['idColaborador'] = owners;              
-        membrosOwners['owners'].push(owners.id);
-        $scope.ownersTime.push(dados);
-        $scope.pesquisa = {};  
-        verificaOwner();  
+        dados['idColaborador'] = membros;
+        membrosOwners['membros'].push(membros.id);
+        $scope.membrosTime.push(dados);
+        $scope.pesquisa = {};
     };
 
 
-    function verificaOwner(){        
+    function adicionarOwner(owners) {
+        let dados = {};
+        let naoPodeAdicionar = membrosOwners['owners'].includes(owners.id) || membrosOwners['membros'].includes(owners.id);
+        if (naoPodeAdicionar) {
+            toastr.error('ja vinculado ao time');
+            return;
+        }
+
+        dados['idColaborador'] = owners;
+        membrosOwners['owners'].push(owners.id);
+        $scope.ownersTime.push(dados);
+        $scope.pesquisa = {};
+        verificaOwner();
+    };
+
+
+    function verificaOwner() {
         console.log($scope.$routeParamsfomularioEditarTime);
-        $scope.verificaOwner = membrosOwners['owners'].length <= 0;        
+        $scope.verificaOwner = membrosOwners['owners'].length <= 0;
     }
 
-    function removerMenbroDoGrupo(colaborador){        
+    function removerMenbroDoGrupo(colaborador) {
         debugger;
-        $scope.membrosTime = $scope.membrosTime.filter(f => f.idColaborador.id != colaborador.idColaborador.id);     
+        $scope.membrosTime = $scope.membrosTime.filter(f => f.idColaborador.id != colaborador.idColaborador.id);
+        membrosOwners['membros'] = membrosOwners['membros'].filter(f => f != colaborador.idColaborador.id);
     }
 
-    function removerOwnerDoGrupo(owner){
+    function removerOwnerDoGrupo(owner) {
         $scope.ownersTime = $scope.ownersTime.filter(o => o.idColaborador.id != owner.idColaborador.id);
+        membrosOwners['owners'] = membrosOwners['owners'].filter(f => f != owner.idColaborador.id);
         verificaOwner()
     }
+
+    function tornarOwner(colaborador) {
+        let dados = {};
+        dados['idColaborador'] = colaborador;
+        removerMenbroDoGrupo(colaborador);
+        membrosOwners['owners'].push(colaborador.idColaborador.id);
+        $scope.ownersTime.push(dados);
+    }
+
+    function tornarMembro(owner) {
+        let dados = {};
+        dados['idColaborador'] = owner;
+        removerOwnerDoGrupo(owner);
+        membrosOwners['membros'].push(owner.idColaborador.id);
+        $scope.membrosTime.push(owner);
+    }
+
 });
 
 // {
