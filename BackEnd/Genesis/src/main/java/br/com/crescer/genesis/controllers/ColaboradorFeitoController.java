6372b787609e96ec156df0bb37dae864eb05ddd9
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-07-12 21:01:52 -0300

    feat(ColaboradorFeito): wip - métodos para perfil de colaborador e home.

--- BackEnd/Genesis/src/main/java/br/com/crescer/genesis/controllers/ColaboradorFeitoController.java
+++ BackEnd/Genesis/src/main/java/br/com/crescer/genesis/controllers/ColaboradorFeitoController.java
@@ -5,19 +5,17 @@
  */
 package br.com.crescer.genesis.controllers;
 
-import br.com.crescer.genesis.entidades.Colaborador;
 import br.com.crescer.genesis.entidades.ColaboradorFeito;
 import br.com.crescer.genesis.services.ColaboradorFeitoService;
 import br.com.crescer.genesis.services.ColaboradorService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -39,16 +37,27 @@ public class ColaboradorFeitoController {
         service.novoColaboradorFeito(colFeito);
     }
 
-    @GetMapping
+    @RequestMapping(value = "home-aut", method = RequestMethod.GET)
     public Iterable<ColaboradorFeito> buscarTodosColaboradoresFeitos(@AuthenticationPrincipal User user) {
-        if (user != null) {
-            Colaborador colab = serviceColaborador.buscarPorEmail(user.getUsername());
-            Iterable<ColaboradorFeito> listaAutenticada = service.buscarTodos(colab);
+            Iterable<ColaboradorFeito> listaAutenticada = service.buscarTodos(user);
             return listaAutenticada;
-        } else {
-            Iterable<ColaboradorFeito> listaPublica = service.feitosPublicos();
+    } 
+    
+    @RequestMapping(value = "home-publica", method = RequestMethod.GET)
+    public Iterable<ColaboradorFeito> buscarTodosColaboradoresFeitosPublicos() {
+            Iterable<ColaboradorFeito> listaPublica = service.buscarTodos(null);
             return listaPublica;
-        }
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Iterable<ColaboradorFeito> buscarFeitosDoColaborador(@AuthenticationPrincipal User user, @PathVariable("id") Long id) {
+        Iterable<ColaboradorFeito> listaAutenticada = service.buscarFeitosPerfil(user, id);
+        return listaAutenticada;
+    }
+    
+    @RequestMapping(value = "perfil/{id}", method = RequestMethod.GET)
+    public Iterable<ColaboradorFeito> buscarFeitosDoColaboradorPublico(@PathVariable("id") Long id) {
+        Iterable<ColaboradorFeito> listaPublica = service.buscarFeitosPerfil(null, id);
+        return listaPublica;
+    }
 }
